5677b9d49a7e685a5bdf94d7f1addf24f4bda0ce
chinmayswami3 <40798090+chinmayswami3@users.noreply.github.com>
2019-06-05 20:17:54 +0530

    Prime Number program
    This program will check if the number entered by the user is prime or not. The user will be asked if he/she wants to continue or quit. So the user can check for as many numbers as he/she wants till he/she decides to quit.

---
+++ Prime.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class Prime
+{
+    public static void main()
+    {
+        Scanner sc=new Scanner(System.in);
+        int i,n,c=1;
+        while(c!=0)
+        {
+            c=0;
+            System.out.print("Enter number to check for Prime: ");
+            n=sc.nextInt();
+            for(i=1;i<=n;i++)
+            {
+                if(n%i==0)
+                {
+                    c=c+1;
+                }
+            }
+            if(c==2)
+            {
+                System.out.println(n+" is a Prime number!");
+            }
+            else
+            {
+                System.out.println(n+" is a Composite number.");
+            }
+            System.out.print("Do you want to try again? Enter any number to continue and enter 0 to exit: ");
+            c=sc.nextInt();
+        }
+    }
+}
